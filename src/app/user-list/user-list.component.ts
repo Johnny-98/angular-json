3cf629b0c00ad47ab8197747ffac4e1280815e91
Peter <petiodjonov@yahoo.com>
2024-01-26 18:30:46 +0200

    Dynamically fetch data from the json

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -7,45 +7,55 @@ import { DataService } from '../data.service';
   styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent implements OnInit {
-  indexes: any = {};
-  demographicSummary: any = {};
+  dataStore: any = {};
+
+  dataKeys:any[] = ["indexes" , "demographicSummary"]
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.dataService.fetchData().subscribe(
       (data) => {
-        if (data.indexes) {
-          for (const key in data.indexes) {
-            if (data.indexes.hasOwnProperty(key) && typeof data.indexes[key] === 'object' && this.isEmptyObject(data.indexes[key])) {
-              data.indexes[key] = 'empty object';
-            }
-          }
-          this.indexes = data.indexes;
-        }
-        if (data.demographicSummary) {
-          this.demographicSummary = data.demographicSummary;
-        }
+        this.handleData(data, this.dataStore);  
       },
       (error) => {
         console.error('Error fetching data:', error);
       }
-    );
+    )
+  }
+
+  private handleData(data: any, targetObject: any): void {
+    if (data && typeof data === 'object') {
+      
+      for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+          let value = data[key];
+          targetObject[key] = value;
+        }
+      }
+    }
   }
 
   renderValue(value: any): string {
+    // Check for empty object specifically
+    if (this.isObject(value) && this.isEmptyObject(value)) {
+      return "novalue"; // Return "novalue" if the object is empty
+    }
+    if (this.isObject(value)) {
+      return JSON.stringify(value, null, 2); // Converts object to formatted JSON string
+    }
     return Array.isArray(value) ? value.join(', ') : String(value);
   }
-
   transformKey(key: unknown): string {
     return String(key).replace('_', ' ');
   }
-
   isObject(value: any): value is Record<string, unknown> {
     return typeof value === 'object' && value !== null;
   }
-
-  private isEmptyObject(obj: any): boolean {
+  isEmptyObject(obj: any): boolean {
     return Object.keys(obj).length === 0;
+  }
+  getPropertyValue(propertyName: string): any {
+    return this.dataStore[propertyName];
   }
 }
